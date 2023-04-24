4dafc75caa1181612b028c1a7c79045fc9674e4e
andiz2 <codeandi@gmail.com>
2023-04-24 17:41:48 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Hello from "./Hello";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
+}
